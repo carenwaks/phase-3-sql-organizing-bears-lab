8171c36ecca4cb6f88a1fe3c7409b99f2b9e95eb
Caren Nafula Wakuloba <canafula16@gmail.com>
2023-05-25 15:57:40 +0300

    Completed assignment

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (id,name,age,sex,color,temperament,alive)
+VALUES(1,'Mr.Chocolate', 22, 'M', 'brown', 'cool', 1),
+(2,'Rowdy',20, 'M', 'beige', 'calm', 1),
+(3,'Tabitha', 12, 'F', 'white', 'cold',0),
+(4,'Sergent Brown', 52,'M', 'black', 'strict', 1),
+(5,'Melissa', 22, 'F', 'blue', 'quiet', 0),
+(6,'Grinch',28, 'M', 'green', 'bored', 1),
+(7,'Wendy', 32, 'F', 'yellow',' happy',0),
+(8,NULL, 2, 'M', 'red', 'meh', 1);
